b2c57e1a404a2755199d49af334496734d602a3c
ganesh-zambare <ganesh.zambare@cognologix.com>
2022-01-09 17:32:12 +0530

    updated calendercode datatype

--- models/edfi/entity/edfi_calender.sql
+++ models/edfi/entity/edfi_calender.sql
@@ -1,29 +1,33 @@
 WITH calendar AS (
-	SELECT
-		uuid_generate_v4() AS resourceid,
+    SELECT
+        uuid_generate_v4() AS resourceid,
         json_build_object(
-            'calendarCode', cb.calendarcode,
+            'calendarCode', cast(cb.calendarcode as varchar),
             'schoolId', cb.schoolid,
             'schoolYear', cb.schoolyear
         )  AS externalid,
-		'CALENDER' AS resourcetype,
+        'CALENDER' AS resourcetype,
         'CREATE' AS operation,
-		0 AS status,
-		json_build_object(
-			'calendarCode', cb.calendarcode,
-			'schoolId', cb.schoolid,
-			'schoolYear', cb.schoolyear,
+        0 AS status,
+        json_build_object(
+            'calendarCode', cast(cb.calendarcode as varchar),
+            'schoolReference', json_build_object(
+                'schoolId', cb.schoolid
+            ),
+            'schoolYearTypeReference', json_build_object(
+                'schoolYear', cb.schoolyear
+            ),
             'calendarTypeDescriptor', cb.calendartypedescriptor
-		) AS payload
-	FROM
-		calendar_base AS cb
+        ) AS payload
+    FROM
+        calendar_base AS cb
 )
 SELECT
     resourceid,
     externalid,
-	resourcetype,
-	operation,
-	payload,
+    resourcetype,
+    operation,
+    payload,
     status
 FROM
     calendar
